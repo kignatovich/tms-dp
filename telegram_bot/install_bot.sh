3be99d662011ded6592f846bb6dc0126dc71c1ef
kignatovich <110161538+kignatovich@users.noreply.github.com>
2023-08-02 11:31:46 +0300

    Update install_bot.sh

--- telegram_bot/install_bot.sh
+++ telegram_bot/install_bot.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-pip3 install python-telegram-bot
+#pip3 install python-telegram-bot
 mkdir -p /opt/infra
 cp ./telegram_bot/tgbot.py /opt/infra
-tail  /etc/systemd/system/tgbp.service << EOF
-[Unit]
-Description=My tg service
-After=multi-user.target
- 
-[Service]
-User=root
-Group=root
-Type=simple
-Restart=always
-ExecStart=/usr/bin/python3 /opt/infra/tgbot.py
- 
-[Install]
-WantedBy=multi-user.target
-EOF
+cp ./telegram_bot/tgbp.service /etc/systemd/system/

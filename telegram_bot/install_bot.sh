1ea50c2dc1c8c2e8b26d81670a069fe7d8d1aa9b
kignatovich <110161538+kignatovich@users.noreply.github.com>
2023-08-02 11:31:56 +0300

    Update install_bot.sh

--- telegram_bot/install_bot.sh
+++ telegram_bot/install_bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-#pip3 install python-telegram-bot
+pip3 install python-telegram-bot
 mkdir -p /opt/infra
 cp ./telegram_bot/tgbot.py /opt/infra
 cp ./telegram_bot/tgbp.service /etc/systemd/system/

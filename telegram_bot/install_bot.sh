2a4dd15268e22147e52356b1191ecbf7dfe6100f
kignatovich <110161538+kignatovich@users.noreply.github.com>
2023-08-02 11:56:05 +0300

    Update install_bot.sh

--- telegram_bot/install_bot.sh
+++ telegram_bot/install_bot.sh
@@ -2,7 +2,7 @@
 TRF=terraform_1.4.6_linux_amd64.zip
 apt update
 apt upgrade -y
-apt install unzip
+apt install unzip python3-pip
 wget https://hashicorp-releases.yandexcloud.net/terraform/1.4.6/$TRF
 unzip $TRF -d ./
 mv ./terraform /usr/local/bin/

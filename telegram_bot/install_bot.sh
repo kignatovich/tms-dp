0f3c814b65ef84cf3cf6eaf89fbc671fbfc3d0b3
kignatovich <110161538+kignatovich@users.noreply.github.com>
2023-08-02 11:32:38 +0300

    Update install_bot.sh

--- telegram_bot/install_bot.sh
+++ telegram_bot/install_bot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-apt update
+apt update 
 apt upgrade -y
 pip3 install python-telegram-bot
 mkdir -p /opt/infra
 cp ./telegram_bot/tgbot.py /opt/infra
 cp ./telegram_bot/tgbp.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable tgbp.service
+sudo systemctl start tgbp.service

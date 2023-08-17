baf4c03c1bf2c942281b0f51f71beb0e70691f04
Kiryl Ihnatovich <110161538+kignatovich@users.noreply.github.com>
2023-08-17 12:14:48 +0300

    Update loki.sh

--- infra/loki.sh
+++ infra/loki.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd loki/
-docker run --name loki -d -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:2.8.0 -config.file=/mnt/config/loki-config.yaml
+docker run --name loki -d -v $(pwd)/tms-dp/infra/loki:/mnt/config -p 3100:3100 grafana/loki:2.8.0 -config.file=/mnt/config/loki-config.yaml
 sleep 5
-docker run --name promtail -d -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:2.8.0 -config.file=/mnt/config/promtail-config.yaml
+docker run --name promtail -d -v $(pwd)/tms-dp/infra/loki:/mnt/config -v /var/log:/var/log --link loki grafana/promtail:2.8.0 -config.file=/mnt/config/promtail-config.yaml
 echo  "Loki is running!!!!!"

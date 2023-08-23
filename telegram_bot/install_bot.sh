dda7c0de402d38ff5b873bb3a5ac793d8a87887e
Kiryl Ihnatovich <110161538+kignatovich@users.noreply.github.com>
2023-08-23 17:11:55 +0300

    Update install_bot.sh
    fix install script

--- telegram_bot/install_bot.sh
+++ telegram_bot/install_bot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+current_dir=$(pwd)
 TRF=terraform_1.4.6_linux_amd64.zip
 apt update
 apt upgrade -y
@@ -7,9 +8,12 @@ wget https://hashicorp-releases.yandexcloud.net/terraform/1.4.6/$TRF
 unzip $TRF -d ./
 mv ./terraform /usr/local/bin/
 pip3 install python-telegram-bot
-PATH1=$(readlink -f tgbot.py)
-sed -i "s|PATH_TO_FILE|$PATH1|g" tgbp.service
+PATH1=$(readlink -f ./telegram_bot/tgbot.py)
+PATH2="${current_dir}/telegram_bot/terraform/create_infra/"
+echo $PATH2
+sed -i "s|PATH_TO_FILE|$PATH1|g" ./telegram_bot/tgbp.service
+sed -i "s|PRPATH|$PATH2|g" ./telegram_bot/tgbot.py
 cp ./telegram_bot/tgbp.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable tgbp.service
-sudo systemctl start tgbp.service
+systemctl daemon-reload
+systemctl enable tgbp.service
+systemctl start tgbp.service

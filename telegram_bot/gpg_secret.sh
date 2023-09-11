493c55a02e8df082addfe29f06c5fdd20b91ef0e
Kiryl Ihnatovich <110161538+kignatovich@users.noreply.github.com>
2023-09-11 20:22:20 +0300

    Update gpg_secret.sh

--- telegram_bot/gpg_secret.sh
+++ telegram_bot/gpg_secret.sh
@@ -8,21 +8,21 @@ if [ "$1" == "--create" ]; then
     echo "Ключи созданы."
 
 elif [ "$1" == "--export" ]; then
-   #Экспорт ключей
+   # Экспорт ключей
    echo "Введите ID ключа"
    read user_input
    gpg --export -a "$user_input" > $user_input-public.key
    gpg --export-secret-key -a "$user_input" > $user_input-private.key
 
 elif [ "$1" == "--import" ]; then
-   #Импорт ключей
+   # Импорт ключей
    echo "Введите ID ключа"
    read user_input
    gpg --import $user_input-public.key
    gpg --import $user_input-private.key
 
 elif [ "$1" == "--enc" ]; then
-       if [ "$2" == "--k" ]; then
+    if [ "$2" == "--k" ]; then
         public_key="$3"
         input_file="$4"
         # Шифрование файла с помощью публичного ключа
@@ -39,11 +39,26 @@ elif [ "$1" == "--dec" ]; then
         encrypted_file="$4"
 
         # Расшифровка файла с помощью приватного ключа
-       gpg --decrypt-files "$encrypted_file" > "${encrypted_file%.gpg}_decrypted"
+        gpg --decrypt-files "$encrypted_file" > "${encrypted_file%.gpg}_decrypted"
         echo "Файл расшифрован."
 
     else
         echo "Использование: gpg_secret.sh --dec --k name_private.key /path/to/file"
+    fi
+
+elif [ "$1" == "--rcs" ]; then
+    if [ "$2" == "--k" ]; then
+        private_key="$3"
+        directory="$4"
+
+        # Расшифровка всех файлов с расширением .gpg в указанном каталоге и подкаталогах
+        find "$directory" -type f -name "*.gpg" | while read encrypted_file; do
+            gpg --decrypt-files "$encrypted_file" > "${encrypted_file%.gpg}_decrypted"
+            echo "Файл $encrypted_file расшифрован."
+        done
+
+    else
+        echo "Использование: gpg_secret.sh --rcs --k name_private.key /path/to/folder"
     fi
 
 else
@@ -51,6 +66,7 @@ else
     echo "gpg_secret.sh --create"
     echo "gpg_secret.sh --enc --k name_public.key /path/to/file"
     echo "gpg_secret.sh --dec --k name_private.key /path/to/file"
+    echo "gpg_secret.sh --rcs --k name_private.key /path/to/folder"
     echo "gpg_secret.sh --export //И введите ID ключа"
     echo "gpg_secret.sh --import //И введите ID ключа"
 fi
